5bf1b88aef4414f87389514db5ecfdab0d048115
gotlium <death-metall@mail.ru>
2013-03-30 23:53:19 +0500

    Staff filter

--- logentry_admin/admin.py
+++ logentry_admin/admin.py
@@ -1,10 +1,13 @@
 # -*- coding:utf-8 -*-
-from django.contrib import admin
+
 from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
-from django.utils.html import escape
-from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.admin import SimpleListFilter
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.utils.html import escape
+from django.contrib import admin
+
 
 class ActionListFilter(SimpleListFilter):
     title = _('action')
@@ -18,15 +21,33 @@ class ActionListFilter(SimpleListFilter):
         )
 
     def queryset(self, request, queryset):
-        return queryset.filter(action_flag=self.value()) if self.value() else queryset
+        return queryset.filter(
+            action_flag=self.value()) if self.value() else queryset
+
+
+class UserListFilter(SimpleListFilter):
+    title = _('User')
+    parameter_name = 'user'
+
+    def lookups(self, request, model_admin):
+        return User.objects.values_list('id', 'username',).filter(
+            is_staff=True)
+
+    def queryset(self, request, queryset):
+        return queryset.filter(
+            user_id=self.value(), user__is_staff=True
+        ) if self.value() else queryset
+
 
 class LogEntryAdmin(admin.ModelAdmin):
     date_hierarchy = 'action_time'
     actions = None
-    readonly_fields = LogEntry._meta.get_all_field_names()
+    readonly_fields = LogEntry._meta.get_all_field_names() + [
+        'object_link', 'action_description']
     list_display_links = ['action_time']
 
     list_filter = [
+        UserListFilter,
         'content_type',
         ActionListFilter
     ]
@@ -38,7 +59,6 @@ class LogEntryAdmin(admin.ModelAdmin):
     ]
 
     list_display = [
-        'id',
         'action_time',
         'user',
         'content_type',
@@ -61,11 +81,14 @@ class LogEntryAdmin(admin.ModelAdmin):
             link = escape(obj.object_repr)
         else:
             ct = obj.content_type
-            link = '<a href="%s">%s</a>' % (
-                reverse('admin:%s_%s_change' % (ct.app_label, ct.model),
-                        args=[obj.object_id]),
-                escape(obj.object_repr),
-            )
+            try:
+                link = '<a href="%s">%s</a>' % (
+                    reverse('admin:%s_%s_change' % (ct.app_label, ct.model),
+                            args=[obj.object_id]),
+                    escape(obj.object_repr),
+                )
+            except:
+                link = ""
         return link
     object_link.allow_tags = True
     object_link.admin_order_field = 'object_repr'
